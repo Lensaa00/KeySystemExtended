ce278bb689e77f9ad533d00330df96bd5ffec64a
qniman <qniman@DESKTOP-4F76F9B>
2024-11-29 11:18:04 +1100

    Добавьте файлы проекта.

---
+++ KeySystemExtended/KSELightActions.cs
@@ -0,0 +1,67 @@
+﻿using MSCLoader;
+using System.Collections;
+using UnityEngine;
+
+namespace KeySystemExtended
+{
+    public class KSELightActions: MonoBehaviour
+    {
+
+        GameObject LeftMarkers;
+        GameObject RightMarkers;
+        GameObject Electrics;
+
+        bool isBlinking = false;
+
+        void Start ()
+        {
+            LeftMarkers = GameObject.Find("SATSUMA(557kg, 248)/Electricity/PowerON/Indicators/Left");
+            if (LeftMarkers == null) ModConsole.Error("[KSELightActions] Left markers not found!");
+            else ModConsole.Print("[KSELightActions] Left markers found!");
+
+            RightMarkers = GameObject.Find("SATSUMA(557kg, 248)/Electricity/PowerON/Indicators/Right");
+            if (LeftMarkers == null) ModConsole.Error("[KSELightActions] Right markers not found!");
+            else ModConsole.Print("[KSELightActions] Right markers found!");
+
+            Electrics = GameObject.Find("SATSUMA(557kg, 248)/CarSimulation/Car/Electrics");
+            if (Electrics == null) ModConsole.Error("[KSELightActions] Electrics not found!");
+            else ModConsole.Print("[KSELightActions] Electrics found!");
+        }
+
+        IEnumerator Blink(int times = 1)
+        {
+            bool electricsOn = Electrics.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmBool("ElectricsOK").Value;
+            Electrics.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmBool("ElectricsOK").Value = true;
+            if (!isBlinking)
+            {
+                for (int i = 0; i < times; i++)
+                {
+                    RightMarkers.SetActive(true);
+                    LeftMarkers.SetActive(true);
+                    yield return new WaitForSeconds(0.3f);
+                    RightMarkers.SetActive(false);
+                    LeftMarkers.SetActive(false);
+                    yield return new WaitForSeconds(0.3f);
+                }
+            }
+            else
+            {
+                RightMarkers.SetActive(true);
+                LeftMarkers.SetActive(true);
+                yield return new WaitForSeconds(0.3f);
+                RightMarkers.SetActive(false);
+                LeftMarkers.SetActive(false);
+                yield return new WaitForSeconds(3f);
+                StartCoroutine(Blink());
+            }
+            if(!electricsOn) 
+                Electrics.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmBool("ElectricsOK").Value = false;
+            yield return null;
+        }
+
+        public void LightBlinker(int times)
+        {
+            StartCoroutine(Blink(times));
+        }
+    }
+}
---
+++ KeySystemExtended/KSESoundActions.cs
@@ -0,0 +1,55 @@
+﻿using MSCLoader;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace KeySystemExtended
+{
+    public class KSESoundActions : MonoBehaviour
+    {
+        private AudioSource audioSource;
+        private ModAudio modAudio = new ModAudio();
+
+        private Dictionary<string, string> soundPaths = new Dictionary<string, string>();
+
+        void Start()
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.volume = 0.25f;
+            audioSource.loop = false;
+            audioSource.clip = null;
+
+            audioSource.spatialBlend = 1f; // Полностью 3D-звук
+            audioSource.minDistance = 1f; // Минимальная дистанция, на которой звук воспроизводится на полной громкости
+            audioSource.maxDistance = 10f; // Максимальная дистанция, на которой звук затихает
+            audioSource.rolloffMode = AudioRolloffMode.Linear; // Линейное затухание громкости
+
+            modAudio.audioSource = audioSource;
+
+            // Инициализация путей к звукам
+            soundPaths = new Dictionary<string, string>
+            {
+                { "player_EngineStarting", "Mods/Assets/KeySystemExtended/Sound/Player/EngineStarting.wav" },
+                { "player_EngineStarted",  "Mods/Assets/KeySystemExtended/Sound/Player/EngineStarted.wav" },
+                { "player_Ping",  "Mods/Assets/KeySystemExtended/Sound/Player/Ping.wav" },
+                { "player_Ping2",  "Mods/Assets/KeySystemExtended/Sound/Player/Ping2.wav" },
+                { "player_Ping3",  "Mods/Assets/KeySystemExtended/Sound/Player/Ping3.wav" },
+                { "car_LockUnlock",        "Mods/Assets/KeySystemExtended/Sound/Car/LockUnlock.wav" }
+            };
+        }
+
+        public void Play(string category, string soundName)
+        {
+            string key = $"{category}_{soundName}";
+            if (soundPaths.TryGetValue(key, out string path))
+            {
+                modAudio.LoadAudioFromFile(path, true, true);
+                modAudio.Play();
+                ModConsole.Print($"[KSE] Played sound on {gameObject.name}: {path}");
+            }
+            else
+            {
+                ModConsole.Print($"[KSE] Sound not found: {key}");
+            }
+        }
+    }
+}
